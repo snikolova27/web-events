1995f1ffff6e43db8a369b6b65db092e2bd90343
Sonya Nikolova <snikolova27@gmail.com>
2024-01-21 13:22:11 +0200

    better the styling yet again :')

--- events/events.php
+++ events/events.php
@@ -20,6 +20,7 @@ $userId = $_SESSION['user_id'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/common.css">
     <title>Events - Web events</title>
 </head>
 <body>
